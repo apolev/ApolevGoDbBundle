5c8175d2d4df60e3d2c468f3adb355f3c6d295bf
Alexey Polev <alex.v.polev@gmail.com>
2016-03-30 23:15:49 +0300

    Add system configuration parameters for connection.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,6 +2,8 @@
 
 namespace Apolev\GoDbBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -12,6 +14,17 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder;
         $rootNode = $treeBuilder->root('godb');
 
+        /** @var NodeDefinition|ArrayNodeDefinition $prototype */
+        $prototype = $rootNode->prototype('array');
+
+        $connectionBuilder = $prototype->children();
+            $connectionBuilder->scalarNode('_adapter')->isRequired()->cannotBeEmpty()->info('pgsql, mysql, mysqlold, pgsql, sqlite')->end();
+            $connectionBuilder->scalarNode('_lazy')->defaultTrue()->cannotBeEmpty()->info('Initialize connection only when first request will be sent.')->end();
+            $connectionBuilder->scalarNode('_prefix')->defaultNull()->info('Set prefix for all queries. See https://github.com/vasa-c/go-db/wiki/prefix for more details.')->end();
+            $connectionBuilder->scalarNode('_debug')->defaultNull()->info('Switch to debug mode. Go to https://github.com/vasa-c/go-db/wiki/debug for more details.')->end();
+            $connectionBuilder->scalarNode('_compat')->defaultFalse()->info('Switch to compatible mode. Go to https://github.com/vasa-c/go-db/wiki/Compat for more details.')->end();
+
+        $connectionBuilder->end();
         return $treeBuilder;
     }
 }
