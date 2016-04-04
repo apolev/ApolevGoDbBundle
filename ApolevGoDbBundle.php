61a5bea18c8fbd8393a0af794af6aed8facd7d16
Alexey Polev <alex.v.polev@gmail.com>
2016-04-04 23:40:07 +0300

    Create databases while booting.

--- ApolevGoDbBundle.php
+++ ApolevGoDbBundle.php
@@ -2,6 +2,8 @@
 
 namespace Apolev\GoDbBundle;
 
+use go\DB\DB;
+use go\DB\Storage;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ApolevGoDbBundle extends Bundle
@@ -9,5 +11,22 @@ class ApolevGoDbBundle extends Bundle
     public function boot()
     {
         \go\DB\autoloadRegister();
+
+        $storage = new Storage();
+        $this->container->set('godb.storage', $storage);
+
+        $databases = $this->container->getParameter('godb');
+
+        if ($databases) {
+            foreach ($databases as $name => $parameters) {
+                $db = DB::create($parameters);
+                $storage->set($db, $name);
+
+                if (!empty($parameters['inject_to_container'])) {
+                    $id = 'godb_' . $name;
+                    $this->container->set($id, $db);
+                }
+            }
+        }
     }
 }
