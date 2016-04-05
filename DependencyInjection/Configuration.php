d662add68e6599e7f8c10f9859a78d87b70ce2d1
Alexey Polev <alex.v.polev@gmail.com>
2016-04-05 20:33:52 +0300

    Add db parameters

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -3,6 +3,7 @@
 namespace Apolev\GoDbBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -26,12 +27,28 @@ class Configuration implements ConfigurationInterface
             ->info('Database adapter')
         ->end();
 
+        $this->addDbParameters($connectionBuilder);
+
         $connectionBuilder->scalarNode('_lazy')->defaultTrue()->cannotBeEmpty()->info('Initialize connection only when first request will be sent.')->end();
         $connectionBuilder->scalarNode('_prefix')->defaultNull()->info('Set prefix for all queries. See https://github.com/vasa-c/go-db/wiki/prefix for more details.')->end();
         $connectionBuilder->scalarNode('_debug')->defaultNull()->info('Switch to debug mode. Go to https://github.com/vasa-c/go-db/wiki/debug for more details.')->end();
         $connectionBuilder->scalarNode('_compat')->defaultFalse()->info('Switch to compatible mode. Go to https://github.com/vasa-c/go-db/wiki/Compat for more details.')->end();
 
         $connectionBuilder->end();
+
         return $treeBuilder;
+    }
+
+    private function addDbParameters(NodeBuilder $builder)
+    {
+        $parameters = [
+            'host', 'username', 'password', 'dbname', 'port', 'socket',
+            'charset', 'options', 'hostaddr', 'connection_timeout', 'sslmode',
+            'service'
+        ];
+
+        foreach ($parameters as $parameter) {
+            $builder->scalarNode($parameter)->defaultNull()->end();
+        }
     }
 }
